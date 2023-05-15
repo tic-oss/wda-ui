5b667ba350db86630beb91a5540b47ad581de088
Harsha9198 <shriharsha.b@comakeit.com>
2023-05-15 11:33:17 +0530

    "updated"

--- src/pages/Designer.js
+++ src/pages/Designer.js
@@ -5,30 +5,20 @@ import ReactFlow, {
   useNodesState,
   useEdgesState,
   Controls,
-  MiniMap,
+updateEdge
+
 } from 'reactflow';
+import { Button } from '@chakra-ui/core';
 import 'reactflow/dist/style.css';
 
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Button,
-  Input,
-  Select,
-  Stack
-} from '@chakra-ui/react'
 import Sidebar from './../components/Sidebar';
 import MyModal from '../components/Modal/MyModal';
 import ColorSelectorNode from './../components/ColorSelectorNode';
 import "./../App.css"
 
 let application_id = 2;
-let database_id=1;
+let database_id = 1;
+let totalnodes = 1
 const getId = (type='') =>{
       if( type === 'Application')
         return `Application_${application_id++}`
@@ -36,9 +26,8 @@ const getId = (type='') =>{
         return `Database_${database_id++}`
       else if ( type === 'Authentication')
         return 'Authentication_1'
-        else if ( type === 'Deployment')
+      else if ( type === 'Deployment')
         return 'Deployment_1'
-      
     return 'Id'
 }
 const nodeTypes = {
@@ -49,14 +38,37 @@ const nodeTypes = {
 const Designer = () => {
   const reactFlowWrapper = useRef(null);
   const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [nodeMap,setNodeMap] = useState(new Map())
   console.log("Nodes",nodes)
   
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
   console.log("Edges",edges)  
+  console.log('NodeMap',nodeMap)
   const [reactFlowInstance, setReactFlowInstance] = useState(null);
   const [Isopen,setopen]=useState(false);
+  const edgeUpdateSuccessful = useRef(true);
 
-  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+  const onConnect = useCallback((params) => {
+    console.log("Connect ",params)
+    setEdges((eds) => addEdge(params, eds))}
+    , []);
+
+  const onEdgeUpdateStart = useCallback(() => {
+    edgeUpdateSuccessful.current = false;
+  }, []);
+
+  const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+    edgeUpdateSuccessful.current = true;
+    setEdges((els) => updateEdge(oldEdge, newConnection, els));
+  }, []);
+
+  const onEdgeUpdateEnd = useCallback((_, edge) => {
+    if (!edgeUpdateSuccessful.current) {
+      setEdges((eds) => eds.filter((e) => e.id !== edge.id));
+    }
+
+    edgeUpdateSuccessful.current = true;
+  }, []);
 
   const onDragOver = useCallback((event) => {
     event.preventDefault();
@@ -75,7 +87,7 @@ const Designer = () => {
   const onDrop = useCallback(
     (event) => {
       event.preventDefault();
-
+      console.log(event)
       const reactFlowBounds = reactFlowWrapper.current.getBoundingClientRect();
       const type = event.dataTransfer.getData('application/reactflow');
       const name = event.dataTransfer.getData('Name')
@@ -97,15 +109,25 @@ const Designer = () => {
        style: { border: "1px solid", padding: "4px 4px" },
       };
 
-      setNodes((nds) => nds.concat(newNode)
-     );
+      setNodeMap((prev)=>new Map(prev.set(newNode.id,totalnodes++)))
+      setNodes((nds) => nds.concat(newNode));
+
     },
     [reactFlowInstance]
   );
 
   const onclick = (e)=>{
-    console.log(e.target.dataset.id)
-    setopen(e.target.dataset.id)
+    const Id= e.target.dataset.id
+    console.log(Id)
+    if(Id){
+      setopen(Id)
+      // let index = nodeMap.get(Id)
+      // let CurrentNode = nodes[index].data
+      // console.log(CurrentNode)
+      // console.log(document.getElementById("appname"))
+      // =CurrentNode.label
+    }
+    
   }
 
   const onChange = (e) => {
@@ -118,36 +140,15 @@ const Designer = () => {
     console.log(Name,Framework,PackageName,ServerPort,ApplicationType)
     console.log("Nodes",nodes)
     console.log(Isopen)
-    setNodes((nds)=>{
-      return nds.map((node) => {
-        console.log(node.id,Isopen,node.id!==Isopen)
-        if (node.id !== Isopen) {
-          console.log(node.id,Isopen)
-        if (node.id !== '2') {
-            return node;
-          }
 
-          
-          return node
-        }
-        const database = e.target.value;
-        return {
-          ...node,
-          data: {
-            ...node.data,
-            database,
-            Framework:Framework,
-            label:Name,
-            PackageName:PackageName,
-            ServerPort:ServerPort,
-            ApplicationType:ApplicationType
-          }
-        }
-      }
-    )
+    let UpdatedNodes=[...nodes]
+    let index = nodeMap.get(Isopen)
+    let CurrentNode = UpdatedNodes[index]
+    console.log(CurrentNode)
+    CurrentNode.data={...CurrentNode.data,Framework:Framework,label:Name,PackageName:PackageName,ServerPort:ServerPort,ApplicationType:ApplicationType}
+    UpdatedNodes[index]=CurrentNode
+    setNodes(UpdatedNodes)
 
-   }
-    )
     setopen(false)
   }
 
@@ -168,6 +169,7 @@ const Designer = () => {
           //   position: { x: 300, y: 50 },
           // },
     ])
+    setNodeMap((prev)=>new Map(prev.set('Application_1',0)))
     
   },[])
 
@@ -188,8 +190,11 @@ const Designer = () => {
             onDragOver={onDragOver}
             onClick={onclick}
             fitView
+            onEdgeUpdate={onEdgeUpdate}
+            onEdgeUpdateStart={onEdgeUpdateStart}
+            onEdgeUpdateEnd={onEdgeUpdateEnd}
           >
-            <MiniMap
+            {/* <MiniMap
         nodeStrokeColor={(n) => {
           
           if (n.type === 'selectorNode') return image;
@@ -199,7 +204,7 @@ const Designer = () => {
           if (n.type === 'selectorNode') return image;
           return '#fff';
         }}
-      />
+      /> */}
             <Controls />
           </ReactFlow>
         </div>
@@ -219,14 +224,4 @@ const Designer = () => {
 };
 
 export default Designer;
-
-
-// const CustomModal = ({Isopen,setopen})=>{
-
-  
-//     return (
-//      <>
-//      </>
-//     )
-//   }
 
