00f643e8a88678be9446cd521f7bfcc4012ce0a6
harika <harikasree.t@comakeit.com>
2023-05-25 11:46:48 +0530

    change

--- src/pages/Designer.js
+++ src/pages/Designer.js
@@ -360,7 +360,7 @@ const Designer = () => {
     setNodes({'UI':
       {
             id: 'UI',
-            type: 'selectorUI',
+            type: 'default',
             data: { label: 'UI',onChange:onChange},
            style: { border: "1px solid", padding: "4px 4px" },
             position: { x: 250, y: 5 },
