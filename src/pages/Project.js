f17b037c6285dd8630daa081ce05dbe26210c741
harika <harikasree.t@comakeit.com>
2023-06-15 17:35:41 +0530

    onclick open ui modal

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,9 +1,7 @@
-import { useEffect, useRef, useState} from "react";
-import ReactFlow, {
-    ReactFlowProvider,
-  } from "reactflow";
+import { useEffect, useRef, useState } from "react";
+import ReactFlow, { ReactFlowProvider } from "reactflow";
 import "reactflow/dist/style.css";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import CustomImageNode from "./Customnodes/CustomImageNode";
 import CustomServiceNode from "./Customnodes/CustomServiceNode";
 import CustomIngressNode from "./Customnodes/CustomIngressNode";
@@ -12,109 +10,235 @@ import CustomMessageBrokerNode from "./Customnodes/CustomMessageBrokerNode";
 import CustomCloudNode from "./Customnodes/CustomCloudNode";
 import CustomLoadNode from "./Customnodes/CustomLoadNode";
 import CustomLocalenvironmentNode from "./Customnodes/CustomLocalenvironmentNode";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Input,
+  Select,
+  Button,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
 
 const readOnlyNodeStyle = {
-    border: '1px solid #ccc',
-    background: '#f0f0f0',
-    color: '#555',
-}
-
-const readOnlyEdgeStyle = {
-    stroke: '#ccc',
+  border: "1px solid #ccc",
+  background: "#f0f0f0",
+  color: "#555",
 };
 
-
+const readOnlyEdgeStyle = {
+  stroke: "#ccc",
+};
 
 const nodeTypes = {
-    selectorNode: CustomImageNode,
-    selectorNode1: CustomServiceNode,
-    selectorNode2: CustomIngressNode,
-    selectorNode3: CustomAuthNode,
-    selectorNode4: CustomMessageBrokerNode,
-    selectorNode5: CustomCloudNode,
-    selectorNode6: CustomLoadNode,
-    selectorNode7: CustomLocalenvironmentNode,
-  };
+  selectorNode: CustomImageNode,
+  selectorNode1: CustomServiceNode,
+  selectorNode2: CustomIngressNode,
+  selectorNode3: CustomAuthNode,
+  selectorNode4: CustomMessageBrokerNode,
+  selectorNode5: CustomCloudNode,
+  selectorNode6: CustomLoadNode,
+  selectorNode7: CustomLocalenvironmentNode,
+};
 
 const Project = () => {
+  const location = useLocation();
+  const [metadata, setmetadata] = useState(location.state);
+  const [nodes, setNodes] = useState([]);
+  const [edges, setEdges] = useState([]);
 
-    const location = useLocation();
-    const [metadata,setmetadata] = useState(location.state);
-    const [nodes,setNodes] = useState([])
-    const [edges,setEdges] = useState([])
-
-    useEffect(()=>{ 
-        const data  = location?.state
-        if(!data){
-            const data= JSON.parse(localStorage.metadata)
-            setmetadata(data) 
-            setNodes(Object.values(data?.nodes))
-            setEdges(Object.values(data?.edges))
-        }
-        else{
-            localStorage.metadata=JSON.stringify(metadata)
-            setNodes(Object.values(metadata?.nodes))
-            setEdges(Object.values(metadata?.edges))
-        }
-    },[])
-
-
-
-    const reactFlowWrapper = useRef(null);
-    const [tooltipData, setTooltipData] = useState(null);
-    const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
-
-    const onElementClick = (event, element) => {
-            event.preventDefault();
-            setTooltipData(element.data.applicationName);
-            setTooltipPosition({ x: element.position.x , y: +element.position.y});
+  useEffect(() => {
+    const data = location?.state;
+    if (!data) {
+      const data = JSON.parse(localStorage.metadata);
+      setmetadata(data);
+      setNodes(Object.values(data?.nodes));
+      setEdges(Object.values(data?.edges));
+    } else {
+      localStorage.metadata = JSON.stringify(metadata);
+      setNodes(Object.values(metadata?.nodes));
+      setEdges(Object.values(metadata?.edges));
     }
-     
-      const CustomTooltip = () => {
-        const tooltipStyle = {
-          position: 'absolute',
-          left: tooltipPosition.x,
-          top: tooltipPosition.y,
-        };
-    
-        return tooltipData ? (
-          <div className="tooltip" style={tooltipStyle}>
-            {tooltipData}
-          </div>
-        ) : null;
-      };
+  }, []);
+
+  const reactFlowWrapper = useRef(null);
+  const [tooltipData, setTooltipData] = useState(null);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+  const [isOpen, setIsOpen] = useState(false);
+  const [nodeType, setNodeType] = useState("");
+  const [applicationName, setApplicationName] = useState("");
+  const [clientFramework, setClientFramework] = useState("");
+  const [packageName, setPackageName] = useState("");
+  const [serverPort, setServerPort] = useState("");
+  const [withExample, setWithExample] = useState("");
+  const [applicationFramework, setApplicationFrameWork] = useState("")
+  const onElementClick = (event, element) => {
+    event.preventDefault();
+    setTooltipData(element.data.applicationName);
+    setTooltipPosition({ x: element.position.x, y: +element.position.y });
+    setIsOpen(true);
+    if (element.data.applicationType === "gateway") {
+      setNodeType("UI");
+      setClientFramework(element.data.clientFramework);
+      setWithExample(element.data.withExample);
+    } else {
+      setNodeType("Service");
+      setApplicationFrameWork(element.data.applicationFramework)
+
+    }
+    console.log(element.data.applicationType, nodeType, "nodeType");
+    setApplicationName(element.data.applicationName);
+    setPackageName(element.data.packageName);
+    setServerPort(element.data.serverPort);
+
+    console.log(element.data);
+    console.log(element.data.applicationName, "data");
+  };
+
+  const CustomTooltip = () => {
+    const tooltipStyle = {
+      position: "absolute",
+      left: tooltipPosition.x,
+      top: tooltipPosition.y,
+    };
+
+    return tooltipData ? (
+      <div className="tooltip" style={tooltipStyle}>
+        {tooltipData}
+      </div>
+    ) : null;
+  };
+  const handleContainerClose = () => {
+    setIsOpen(false);
+  };
 
   return (
-    <div className="dndflow">
-      <ReactFlowProvider>
-      <div
-        className="reactflow-wrapper"
-        ref={reactFlowWrapper}
-        style={{ width: "100%", height: "90%" }}
-      >
-        <ReactFlow
-            nodes={nodes}
-            edges={edges}
-            onEdgeClick={onElementClick}
-            onNodeClick={onElementClick}
-            nodesConnectable={false}
-            elementsSelectable={false}
-            nodesDraggable={false}
-            panOnDrag={false}
-            fitView
-            nodeTypes={{
+    <>
+      <Modal isOpen={isOpen} onClose={handleContainerClose} isCentered={true}>
+        <ModalOverlay />
+        <ModalContent>
+          {nodeType === "UI" ? (
+            <ModalHeader>UI</ModalHeader>
+          ) : (
+            <ModalHeader>Service</ModalHeader>
+          )}
+          <ModalCloseButton />
+          <ModalBody>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "Left",
+              }}
+            >
+              <FormControl>
+                <FormLabel>Application name</FormLabel>
+                <Input
+                  mb={4}
+                  variant="outline"
+                  id="applicationName"
+                  placeholder="Name"
+                  borderColor={"black"}
+                  value={applicationName}
+                />
+              </FormControl>
+              {nodeType === "UI" && (
+                
+              <FormControl>
+                <FormLabel>Client Framework</FormLabel>
+                <Select
+                  mb={4}
+                  variant="outline"
+                  id="clientFramework"
+                  borderColor={"black"}
+                  value={clientFramework}
+                >
+                 
+                </Select>
+              </FormControl>
+              )}
+
+              <FormControl>
+                <FormLabel>Package Name</FormLabel>
+                <Input
+                  mb={4}
+                  variant="outline"
+                  id="packageName"
+                  placeholder="packageName"
+                  borderColor={"black"}
+                  value={packageName}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Server Port</FormLabel>
+                <Input
+                  mb={4}
+                  variant="outline"
+                  id="serverPort"
+                  placeholder="serverPort"
+                  borderColor={"black"}
+                  value={serverPort}
+                />
+              </FormControl>
+              {nodeType === "UI" && (
+              <FormControl>
+                <FormLabel>Want to have an Example</FormLabel>
+                <Select
+                  mb={4}
+                  variant="outline"
+                  id="withExample"
+                  borderColor={"black"}
+                  value={withExample}
+                >
+                </Select>
+              </FormControl>
+              )}
+            </div>
+            <Button
+              // onClick={() => onSubmit(element.data)}
+              style={{ display: "block", margin: "0 auto" }}
+              disabled={true}
+            >
+              Submit
+            </Button>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <div className="dndflow">
+        <ReactFlowProvider>
+          <div
+            className="reactflow-wrapper"
+            ref={reactFlowWrapper}
+            style={{ width: "100%", height: "90%" }}
+          >
+            <ReactFlow
+              nodes={nodes}
+              edges={edges}
+              onEdgeClick={onElementClick}
+              onNodeClick={onElementClick}
+              nodesConnectable={false}
+              elementsSelectable={false}
+              nodesDraggable={false}
+              panOnDrag={false}
+              fitView
+              nodeTypes={{
                 customReadOnlyNode: readOnlyNodeStyle,
-                ...nodeTypes
+                ...nodeTypes,
               }}
               edgeTypes={{
                 customReadOnlyEdge: readOnlyEdgeStyle,
               }}
-        />
-        <CustomTooltip />
-        </div>
-      </ReactFlowProvider>
-    </div>
-  )
-}
+            />
+            <CustomTooltip />
+          </div>
+        </ReactFlowProvider>
+      </div>
+    </>
+  );
+};
 
-export default Project
+export default Project;
