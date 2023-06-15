ca42d815778128b48b55045fa703bbdfd473668e
Harsha9198 <114900201+Harsha9198@users.noreply.github.com>
2023-06-15 18:56:19 +0530

    Mindmap2 (#61)
    Toast changes

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -1,19 +1,51 @@
+import React, { useEffect } from "react";
 import { useKeycloak } from "@react-keycloak/web";
-import { useHistory } from "react-router-dom";
+import { useToast, Box, Text, Icon } from "@chakra-ui/react";
+import { MdWarning } from "react-icons/md";
+import { Route, useHistory, useLocation, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ children }) => {
-const { keycloak } = useKeycloak();
-const history = useHistory();
+const PrivateRoute = ({ children, ...rest }) => {
+  const { keycloak } = useKeycloak();
+  const toast = useToast();
+  const isLoggedIn = keycloak.authenticated;
+  const location = useLocation();
+  const history = useHistory();
 
-const isLoggedIn = keycloak.authenticated;
+  useEffect(() => {
+    if (!isLoggedIn && location.pathname !== "/") {
+      toast({
+        render: () => (
+          <Box
+            backgroundColor="orange"
+            color="white"
+            p={4}
+            borderRadius="md"
+            boxShadow="md"
+            display="flex"
+            alignItems="center"
+          >
+            <Icon as={MdWarning} boxSize={6} mr={2} />
+            <Text fontSize="lg">Please log in to access this route.</Text>
+          </Box>
+        ),
+        duration: 3000,
+        isClosable: true,
+        position: "top-right",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "400px",
+      });
 
-if (!isLoggedIn) {
-alert("Please login to continue.");
-history.replace("/");
-return null;
-}
+      return; // Prevent rendering the private route content
+    }
+  }, [isLoggedIn, toast, location]);
 
-return children;
+  if (isLoggedIn || location.pathname === "/") {
+    return <Route {...rest}>{children}</Route>;
+  }
+
+  return <Redirect to="/" />;
 };
 
 export default PrivateRoute;
