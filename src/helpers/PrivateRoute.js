479fc7cb95372343a20ca15cccf057f9edefea7e
Manideep <manideep.a@comakeit.com>
2023-06-16 21:57:10 +0530

    updated keycloak to redirct to login page

--- src/helpers/PrivateRoute.js
+++ src/helpers/PrivateRoute.js
@@ -1,50 +1,19 @@
 import React, { useEffect } from "react";
 import { useKeycloak } from "@react-keycloak/web";
-import { useToast, Box, Text, Icon } from "@chakra-ui/react";
-import { MdWarning } from "react-icons/md";
-import { Route, useHistory, useLocation, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 const PrivateRoute = ({ children, ...rest }) => {
   const { initialized, keycloak } = useKeycloak();
-  const toast = useToast();
-  const isLoggedIn = keycloak.authenticated;
-  const location = useLocation();
-  const history = useHistory();
 
   useEffect(() => {
     if(initialized){
       if (!keycloak.authenticated) {
-        toast({
-          render: () => (
-            <Box
-              backgroundColor="orange"
-              color="white"
-              p={4}
-              borderRadius="md"
-              boxShadow="md"
-              display="flex"
-              alignItems="center"
-            >
-              <Icon as={MdWarning} boxSize={6} mr={2} />
-              <Text fontSize="lg">Please log in to access this route.</Text>
-            </Box>
-          ),
-          duration: 3000,
-          isClosable: true,
-          position: "top-right",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: "400px",
-        }); 
-        history.replace('/')
-      }
-  }}, [initialized,keycloak,isLoggedIn, toast, location]);
-
+         keycloak.login()
+      }}});
   if (keycloak.authenticated) {
      return <Route {...rest}>{children}</Route>;
   }
-    return null
+  return null;
 };
 
 export default PrivateRoute;
