6ab0487ec17cb953c674d290b1fc04ab6b192eec
harika <harikasree.t@comakeit.com>
2023-07-21 17:31:21 +0530

    infra modal view in projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,6 +8,7 @@ import {
   Td,
   TableContainer,
   Button,
+  Modal,
 } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { useKeycloak } from "@react-keycloak/web";
@@ -15,26 +16,88 @@ import azure from "../../src/assets/Azure.png";
 import aws from "../../src/assets/aws.png";
 import minikube from "../../src/assets/mini.jpeg";
 import Footer from "../components/Footer";
+import DeploymentModal from "../components/Modal/DeploymentModal";
 
 function Projects() {
   const history = useHistory();
 
   const [data, setData] = useState([]);
   const { keycloak, initialized } = useKeycloak();
+  const [showData, setShowData] = useState(false);
+  const [modalData, setModalData] = useState([]);
+  const [cloudName, setCloudName] = useState("");
 
-  const handleClick = (data, column, name) => {
-    console.log(data);
+  const DefaultData = {
+    awsAccountId: "",
+    awsRegion: "",
+    kubernetesStorageClassName: "",
+
+    clusterName: "",
+    deploymentType: "",
+    ingressDomain: "",
+    ingressType: "",
+    k8sWebUI: "",
+    kubernetesNamespace: "",
+    kubernetesUseDynamicStorage: "",
+    monitoring: "",
+
+    azureLocation: "",
+    subscriptionId: "",
+    tenantId: "",
+
+    dockerRepositoryName: "",
+  };
+
+  const [depData, setDepData] = useState(DefaultData);
+
+  const handleClick = async (data, column, name) => {
     if (column === "Architecture")
       history.push({
         pathname: "/projects/" + name,
         state: data,
       });
-    else
-      history.push({
-        pathname: "/projects/" + name,
-        state: data,
-      });
+    else {
+      if (data) {
+        if (data.cloudProvider === "aws") {
+          setCloudName("aws");
+          setDepData((prev) => ({
+            ...prev,
+            awsAccountId: data?.awsAccountId,
+            awsRegion: data?.awsRegion,
+            kubernetesStorageClassName: data?.kubernetesStorageClassName,
+          }));
+        } else if (data.cloudProvider === "azure") {
+          setCloudName("azure");
+          setDepData((prev) => ({
+            ...prev,
+            azureLocation: data?.azureLocation,
+            subscriptionId: data?.subscriptionId,
+            tenantId: data?.tenantId,
+          }));
+        } else {
+          setCloudName("minikube");
+          setDepData((prev) => ({
+            ...prev,
+            dockerRepositoryName: depData?.dockerRepositoryName,
+          }));
+        }
+        setDepData((prev) => ({
+          ...prev,
+          clusterName: data?.clusterName,
+          deploymentType: data?.deploymentType,
+          ingressDomain: data?.ingressDomain,
+          ingressType: data?.ingressType,
+          k8sWebUI: data?.k8sWebUI,
+          kubernetesNamespace: data?.kubernetesNamespace,
+          kubernetesUseDynamicStorage: data?.kubernetesUseDynamicStorage,
+          monitoring: data?.monitoring,
+        }));
+        await setShowData(true);
+        await setModalData(() => ({ ...data }));
+      }
+    }
   };
+
   useEffect(() => {
     if (initialized) {
       fetch(process.env.REACT_APP_API_BASE_URL + "/api/blueprints", {
@@ -51,6 +114,10 @@ function Projects() {
         .catch((error) => console.error(error));
     }
   }, [initialized, keycloak]);
+
+  const handleContainerClose = () => {
+    setShowData(false);
+  };
 
   return (
     <div>
@@ -130,6 +197,30 @@ function Projects() {
           </Tbody>
         </Table>
       </TableContainer>
+      {showData && (
+        <>
+          <DeploymentModal
+            cloudModal={true}
+            cloudName={cloudName}
+            handleContainerClose={handleContainerClose}
+            awsAccountId={depData.awsAccountId}
+            awsRegion={depData.awsRegion}
+            kubernetesStorageClassName={depData.kubernetesStorageClassName}
+            azureLocation={depData.azureLocation}
+            subscriptionId={depData.subscriptionId}
+            tenantId={depData.tenantId}
+            clusterName={depData.clusterName}
+            deploymentType={depData.deploymentType}
+            ingressDomain={depData.ingressDomain}
+            ingressType={depData.ingressType}
+            k8sWebUI={depData.k8sWebUI}
+            kubernetesNamespace={depData.kubernetesNamespace}
+            kubernetesUseDynamicStorage={depData.kubernetesUseDynamicStorage}
+            monitoring={depData.monitoring}
+            dockerRepositoryName={depData.dockerRepositoryName}
+          />
+        </>
+      )}
       <Footer />
     </div>
   );
