868cab0fa48b8e20e69b7d30169e89467d618037
Kavitha Thota <kavitha.t@comakeit.com>
2023-10-31 20:15:34 +0530

    added config files for sonarqube

--- src/components/pages/DocHome/DocHome.test.js
+++ src/components/pages/DocHome/DocHome.test.js
@@ -1,20 +1,18 @@
-import { React, render, unmountComponentAtNode } from 'react-dom';
+import { React } from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import DocHome from './DocHome';
 
 describe('DocHome component', () => {
     it('should open the documentation URL in a new window and redirect', () => {
-       // const openSpy = jest.spyOn(window, 'open');
+        // const openSpy = jest.spyOn(window, 'open');
 
         const div = document.createElement('div');
         document.body.appendChild(div);
-
-        render(<DocHome />, div);
-
-        // Your assertions here (e.g., checking if the openSpy was called)
-
-        unmountComponentAtNode(div);
+        const root = createRoot(div);
+    root.render(<DocHome />);
+        root.unmount();
         div.remove();
     });
-    
+
 });
 
