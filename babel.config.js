2d6caed999a117441f72919ce65607a12899e817
Kavitha Thota <kavitha.t@comakeit.com>
2023-10-31 12:32:24 +0530

    added sonar scanner properties

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+      // Other Babel presets you might be using
+      '@babel/preset-env',{ "targets": "node 14" },
+      '@babel/preset-react',
+      '@babel/preset-typescript', // Add this line
+    ],
+    // Other Babel configuration options (if needed)
+  };
+  
